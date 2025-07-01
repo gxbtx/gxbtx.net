07d85d840d3fcea41e6a055b6d48137749b7269e
Lilith Strife <jasonc8484@gmail.com>
2025-07-01 18:50:18 -0500

    Update make_header_footer.js

--- scripts/make_header_footer.js
+++ scripts/make_header_footer.js
@@ -10,7 +10,7 @@ file, instead of editing every html page when you want to change a link
 // You can use internal (index.html) links to point to pages you have in this website
 // or external links (like if you have a store or patreon)
 document.querySelector("#header").innerHTML = `
-<div id="img">
+<div id="headerImg">
 <img src="./images/logo.png"/>
 </div>
 <div id="navLinks">
